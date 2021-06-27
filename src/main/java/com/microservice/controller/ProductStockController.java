0918b1fb9321c9b903d9f61dbc71b3789c7ab306
Vinit Pandey <vinitpandhey@gmail.com>
2021-06-27 13:52:18 +0530

    Injected Swagger

--- src/main/java/com/microservice/controller/ProductStockController.java
+++ src/main/java/com/microservice/controller/ProductStockController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.beans.ProductStockBean;
@@ -18,6 +20,12 @@ public class ProductStockController {
 	
 	@Autowired
 	Environment environment;
+	
+	@PostMapping("/saveData")
+	public void saveData(@RequestBody ProductStock ps)
+	{
+		productStockRepository.save(ps);
+	}
 	
 	@GetMapping("/check-product-stock/productName/{productName}/productAvailability/{productAvailability}")
 	public ProductStockBean checkProductStock(@PathVariable String productName,
@@ -37,5 +45,13 @@ public class ProductStockController {
 
 	    return productStockBean;
 	}
+	
+	@GetMapping("/getData/{id}")
+	public ProductStock getProduct(@PathVariable Long id) {
+		
+		return productStockRepository.getById(id);
+		
+	}
+	
 
 }
--- src/main/java/com/microservice/entity/ProductStock.java
+++ src/main/java/com/microservice/entity/ProductStock.java
@@ -4,11 +4,15 @@ import java.math.BigDecimal;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="PRODUCT_STOCK")
 public class ProductStock {
 	@Id
+	@GeneratedValue
 	private Long id;
 	@Column(name="product_name")
 	private String productName;
